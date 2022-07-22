23176d0892c2cf5123ac3dadb9a093129ea19dac
CommanderBaman <shubhamojha18@gmail.com>
2022-07-22 17:47:12 +0530

    build: add basic background structure

---
+++ components/CustomBackground.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="custom-background-wrapper">
+    <div
+      v-for="(radius, index) in circleRadii"
+      :key="index"
+      class="circle-container"
+    >
+      <ShapeColoredCircle
+        :radius="radius"
+        :color="'white'"
+        :position-x="positionXArray[index]"
+        :position-y="positionYArray[index]"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { getRandomInteger } from '~/tools/DataHandler'
+
+export default {
+  data() {
+    let total = 100
+    const circleRadii = []
+    const positionXArray = []
+    const positionYArray = []
+    while (total > 0) {
+      const radii = getRandomInteger(0, total)
+      const positionX = getRandomInteger(0, 100)
+      const positionY = getRandomInteger(0, 100)
+      total -= radii
+      circleRadii.push(radii)
+      positionXArray.push(positionX)
+      positionYArray.push(positionY)
+    }
+    return {
+      circleRadii,
+      positionXArray,
+      positionYArray,
+    }
+  },
+}
+</script>
+
+<style scoped>
+@import '~/assets/styles/components/customBackground.css';
+</style>
---
+++ components/shape/ColoredCircle.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="colored-circle-container">
+    Radius: {{ $props.radius }} Color: {{ $props.color }} X:
+    {{ $props.positionX }} Y: {{ $props.positionY }}
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    radius: {
+      type: Number,
+      required: true,
+    },
+    color: {
+      type: String,
+      required: true,
+    },
+    positionX: {
+      type: Number,
+      required: true,
+    },
+    positionY: {
+      type: Number,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style scoped>
+@import '~/assets/styles/components/shape/coloredCircle.css';
+</style>
