c7db4b94f9828713750d61cf3e83e2ad6fe2f911
CommanderBaman <shubhamojha18@gmail.com>
2022-09-18 00:38:44 +0530

    style: complete background styling

--- components/CustomBackground.vue
+++ components/CustomBackground.vue
@@ -10,6 +10,7 @@
           position: 'absolute',
           top: `${positionXArray[index]}%`,
           left: `${positionYArray[index]}%`,
+          animationDuration: `${animationDurationArray[index]}s`,
         }"
         :radius="radius"
         :color="'var(--clr-primary)'"
@@ -23,23 +24,27 @@ import { getRandomInteger } from '~/tools/DataHandler'
 
 export default {
   data() {
-    let total = 300
+    let total = 700
     const circleRadii = []
     const positionXArray = []
     const positionYArray = []
+    const animationDurationArray = []
     while (total > 0) {
       const radii = getRandomInteger(0, Math.min(total, 30))
       const positionX = getRandomInteger(0, 100)
       const positionY = getRandomInteger(0, 100)
+      const animationDuration = getRandomInteger(100, 1000) / 100
       total -= radii
       circleRadii.push(radii)
       positionXArray.push(positionX)
       positionYArray.push(positionY)
+      animationDurationArray.push(animationDuration)
     }
     return {
       circleRadii,
       positionXArray,
       positionYArray,
+      animationDurationArray,
     }
   },
 }
