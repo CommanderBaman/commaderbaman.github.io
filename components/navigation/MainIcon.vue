b91f6d36fed2d27c8754bcf20cf2cec1a0ee0db0
CommanderBaman <shubhamojha18@gmail.com>
2022-12-03 14:43:20 +0530

    refactor: navigation components

---
+++ components/navigation/MainIcon.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="nav-link-wrapper">
+    <h3 class="nav-link">
+      <fa :icon="$props.icon" />
+      <NuxtLink :to="$props.routeLink">{{ $props.routeName }}</NuxtLink>
+    </h3>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    icon: {
+      type: Array,
+      required: true,
+    },
+    routeLink: {
+      type: String,
+      required: true,
+    },
+    routeName: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style>
+@import '~/assets/styles/components/navigation/mainIcon.css';
+</style>
