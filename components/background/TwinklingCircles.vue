59df6cc2c14885819b15569fd7879ff92464ad7d
CommanderBaman <shubhamojha18@gmail.com>
2022-12-03 18:10:19 +0530

    style: change background to get smaller circles

--- components/background/TwinklingCircles.vue
+++ components/background/TwinklingCircles.vue
@@ -13,7 +13,7 @@
           animationDuration: `${animationDurationArray[index]}s`,
         }"
         :radius="radius"
-        :color="'var(--clr-primary)'"
+        :color="'var(--clr-on-bg)'"
       />
     </div>
   </div>
@@ -22,15 +22,22 @@
 <script lang="ts">
 import { getRandomInteger } from '~/tools/DataHandler'
 
+const SUM_OF_ALL_RADII = 150
+const MAX_RADII = 5
+
 export default {
   data() {
-    let total = 700
+    // making it so that the total is smaller for smaller screens
+    let total =
+      (SUM_OF_ALL_RADII * window.screen.height * window.screen.width) /
+      1920 /
+      1080
     const circleRadii = []
     const positionXArray = []
     const positionYArray = []
     const animationDurationArray = []
     while (total > 0) {
-      const radii = getRandomInteger(0, Math.min(total, 30))
+      const radii = getRandomInteger(0, Math.min(total, MAX_RADII))
       const positionX = getRandomInteger(0, 100)
       const positionY = getRandomInteger(0, 100)
       const animationDuration = getRandomInteger(100, 1000) / 100
