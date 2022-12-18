baf41e4b58a8f442adfe8efd5ec7bc166c848be3
CommanderBaman <shubhamojha18@gmail.com>
2022-12-18 15:05:02 +0530

    build: add round dot icon

---
+++ components/icon/RoundDot.vue
@@ -0,0 +1,26 @@
+<template>
+  <a :href="$props.iconLink" target="_blank" class="dot-icon-wrapper">
+    <div class="dot-container">
+      <fa class="dot-icon" :icon="$props.iconType" />
+    </div>
+  </a>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    iconType: {
+      type: Array,
+      required: true,
+    },
+    iconLink: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style scoped>
+@import '~/assets/styles/components/icon/roundDot.css';
+</style>
